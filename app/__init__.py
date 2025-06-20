28a26816db9f78916ea779a80d97c19a8c97c085
Alex-Nouriev <140788024+Alex-Nouriev@users.noreply.github.com>
2025-06-20 14:21:19 +0300

    Update __init__.py

--- app/__init__.py
+++ app/__init__.py
@@ -2,11 +2,15 @@ from flask import Flask
 from .routes import bp
 from .metrics import setup_metrics
 
+
+
 def create_app():
     app = Flask(__name__)
     app.register_blueprint(bp)
     setup_metrics(app)
     return app
+
+
 
 if __name__ == '__main__':
     create_app().run(host='0.0.0.0', port=5000)
