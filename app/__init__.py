c8a35ef54f477de1bd00432bb4bac32060a04d2b
Meir Garada <104436031+MeirG9@users.noreply.github.com>
2025-06-22 05:53:41 +0300

    Update __init__.py

--- app/__init__.py
+++ app/__init__.py
@@ -1,16 +1,17 @@
-from flask import Flask
+from flask import Flask, jsonify
 from .routes import bp
 from .metrics import setup_metrics
-
-
 
 def create_app():
     app = Flask(__name__)
     app.register_blueprint(bp)
     setup_metrics(app)
+
+    @app.route('/')
+    def home():
+        return jsonify({'status': 'ok'}), 200
+
     return app
-
-
 
 if __name__ == '__main__':
     create_app().run(host='0.0.0.0', port=5000)
